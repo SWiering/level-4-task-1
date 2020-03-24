ca737875c53488642ae3f1684780e70a1de5d55c
Simon Wiering <simon.wiering@hotmail.com>
2020-03-24 15:45:43 +0100

    first commit

---
+++ app/src/main/java/com/simon/shoppinglist/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.simon.shoppinglist
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private val products = arrayListOf<Product>()
+    private val productAdapter = ProductAdapter(products)
+    private lateinit var productRepository: ProductRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        productRepository = ProductRepository(this)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        rvProducts.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rvProducts.adapter = productAdapter
+        rvProducts.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+//        createItemTouchHelper().attachToRecyclerView(rvProducts)
+        getShoppingListFromDatabase()
+        fab.setOnClickListener { addProduct() }
+    }
+
+    private fun validateFields(): Boolean {
+        return if (productField.text.toString().isNotBlank() && quantityField.text.toString().isNotBlank()) {
+            true
+        } else {
+            Toast.makeText(this, R.string.error_message, Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    private fun addProduct() {
+        if (validateFields()) {
+            mainScope.launch {
+                val product = Product(
+                    name = productField.text.toString(),
+                    quantity = quantityField.text.toString().toInt()
+                )
+
+                withContext(Dispatchers.IO) {
+                    productRepository.insertProduct(product)
+                }
+
+                getShoppingListFromDatabase()
+            }
+        }
+    }
+
+    private fun deleteShoppingList() {
+        mainScope.launch {
+            withContext(Dispatchers.IO) {
+                productRepository.deleteAllProducts()
+            }
+            getShoppingListFromDatabase()
+        }
+    }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@MainActivity.products.clear()
+            this@MainActivity.products.addAll(shoppingList)
+            this@MainActivity.productAdapter.notifyDataSetChanged()
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_delete_shopping_list -> {
+                deleteShoppingList()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/simon/shoppinglist/ProductAdapter.kt
@@ -0,0 +1,27 @@
+package com.simon.shoppinglist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.product_item.view.*
+
+class ProductAdapter(private val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(products[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: Product) {
+            itemView.productName.text = product.name
+            itemView.productQuantity.text = product.quantity.toString()
+        }
+    }
+}
